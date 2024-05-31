a2b57549d3f9c55c0ad832294eeb4beee0381c91
BaldaniyaParth <parthbaldaniya35678@gmail.com>
2024-05-31 21:42:43 +0530

    Day3 Interview Preparation

---
+++ Day3/Java/SumOfAllDigits.java
@@ -0,0 +1,24 @@
+public class SumOfAllDigits {
+
+    static void sumOfAllDigits(int inputNumber)
+    {
+        int copyOfInputNumber = inputNumber;
+
+        int sum = 0;
+
+        while (copyOfInputNumber != 0)
+        {
+            int lastDigit = copyOfInputNumber%10;
+
+            sum = sum + lastDigit;
+
+            copyOfInputNumber = copyOfInputNumber/10;
+        }
+
+        System.out.println("Sum Of All Digits In "+inputNumber+" = "+sum);
+    }
+
+    public static void main(String[] args) {
+        sumOfAllDigits(47862);
+    }
+}
